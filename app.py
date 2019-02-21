5949ecde87938756ed4c60ffe7a590cc8309a1c6
Liam Bates <liambates@gmail.com>
2019-02-21 10:48:23 -0600

    Refactored code and added message flashing

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 """Web app to save maintenance schedule of a users car"""
 
 from functools import wraps
-from flask import Flask, redirect, render_template, request, session
+
+from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
-
 
 from models import Car, Odometer, User, db
 
@@ -40,102 +40,115 @@ def login_required(f):
 @app.route('/', methods=['GET', 'POST'])
 def index():
     """Index route"""
-    # Check if a standard GET request
+    # If a GET request
     if request.method == 'GET':
-        # If user already signed in redirect to the cars route
+        # Redirect to the home route if the user already logged in
         if session.get("user_id"):
             return redirect('/cars')
 
-        # If so render the landing page
+        # Render the landing page
         return render_template('index.html')
 
-    # Ensure user provided email and password on form
+    # If a POST request ensure user provided email and password on form
     if not request.form['email'] or not request.form['password']:
-        return "Error: No username and/or password."
+        flash(u'No username and/or password provided', 'danger')
 
-    # Query the DB for a matching email address and save it as user object
-    user = User.query.filter(User.email == request.form['email']).first()
-
-    # Check if user is blocked
-    if user and user.blocked:
-        return "ERROR: User Blocked"
-
-    # Check that a user was found and password correct
-    if not user or not check_password_hash(user.password_hash,
-                                           request.form['password']):
-        # If failed check if username exists
-        if user and user.failed_logins == 4:
-            # Block the user if this is their
-            user.blocked = True
-        # Else if failed_logins less than 4 increment
-        elif user and user.failed_logins < 4:
-            user.failed_logins += 1
-        # Commit to DB
-        db.session.commit()
-        # Return error
-        return "invalid username and/or password"
     else:
-        # Save user id as session user id
-        session["user_id"] = user.user_id
-        # Reset failed login attempts
-        user.failed_logins = 0
-        # Commit to DB
-        db.session.commit()
-        return redirect('/cars')
+        # Query the DB for a matching email address and save it as user object
+        user = User.query.filter(User.email == request.form['email']).first()
+
+        # Check if user
+        if not user:
+            flash(u'Incorrect username and/or password provided', 'danger')
+        else:
+            # If user is blocked flash a message to warn them
+            if user.blocked:
+                flash(u'Account Blocked', 'danger')
+            else:
+                # Check that password correct
+                if not check_password_hash(user.password_hash,
+                                           request.form['password']):
+                    # Record failed attempt
+                    user.failed_logins += 1
+                    # Block the user if this is their 5th attempt
+                    if user and user.failed_logins >= 5:
+                        user.blocked = True
+                    # Commit to DB
+                    db.session.commit()
+                    # Return error
+                    flash(u'Incorrect username and/or password provided',
+                          'danger')
+                # Correct password provided
+                else:
+                    # Save user id as session user id
+                    session["user_id"] = user.user_id
+                    # Reset failed login attempts
+                    user.failed_logins = 0
+                    # Commit to DB
+                    db.session.commit()
+                    # Direct to home landing page
+                    return redirect('/cars')
+
+    # Return index page again
+    return redirect('/')
 
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    """Registration page route"""
-    # Check if a standard request and render registration page if so
+    """ Registration page to allow users to create an account on the app. """
+
+    # Check if a GET request and render registration page if so
     if request.method == 'GET':
         return render_template('register.html')
 
-    # Ensure the user has submitted all of the required fields
-    if not request.form['email']:
-        return "ERROR: No email address provided"
-    if not request.form['name']:
-        return "ERROR: No name provided"
-    if not request.form['password']:
-        return "ERROR: No password provided"
-    if not request.form['confirm']:
-        return "ERROR: No password confirmation provided"
+    # Otherwise treat as POST and ensure user has submitted all required fields
+    if any(field is '' for field in [
+            request.form['email'], request.form['name'],
+            request.form['password'], request.form['confirm']
+    ]):
+        flash(u'Missing required field/s for new user account', 'danger')
 
-    # Check that password and password match
-    if request.form['password'] != request.form['confirm']:
-        return "ERROR: Password and password confirmation do not match"
+    else:
+        # Check that password and password match
+        if request.form['password'] != request.form['confirm']:
+            flash(u'Password and password confirmation do not match', 'danger')
+        else:
+            # Ensure no other registered users have that email
+            if User.query.filter(User.email == request.form['email']).count():
+                flash(u'Email already registered ', 'danger')
+            else:
+                # Create a new User object with a hashed password
+                user = User(
+                    email=request.form['email'],
+                    name=request.form['name'],
+                    password_hash=generate_password_hash(
+                        request.form['password']))
 
-    # Ensure no other registered users have that email
-    if User.query.filter(User.email == request.form['email']).count():
-        return "ERROR: Email already registered"
+                # Add to DB session
+                db.session.add(user)
+                # Commit to the DB
+                db.session.commit()
 
-    # Create a new User object with a hashed password
-    user = User(
-        email=request.form['email'],
-        name=request.form['name'],
-        password_hash=generate_password_hash(request.form['password']))
+                # Obtain new user model from the db
+                user = User.query.filter(
+                    User.email == request.form['email']).first()
 
-    # Add to DB session
-    db.session.add(user)
-    # Commit to the DB
-    db.session.commit()
+                # Start a new user session
+                session["user_id"] = user.user_id
+                db.session.commit()
 
-    # Obtain new user model from the db
-    user = User.query.filter(User.email == request.form['email']).first()
+                # Redirect to the vehicle landing page
+                return redirect('/cars')
 
-    # Start new session
-    session["user_id"] = user.user_id
-    db.session.commit()
-
-    return redirect('/cars')
+    return redirect('/register')
 
 
 @app.route('/cars', methods=['GET', 'POST'])
 @login_required
 def cars():
+    """ Home landing page for users. Showing a table of their vehicles """
 
     # Query db for users info and cars
-    # TODO: Add method for the user to pull in all of their cars
     cars = Car.query.filter(Car.user_id == session["user_id"]).all()
     user = User.query.filter(User.user_id == session["user_id"]).first()
 
@@ -145,25 +158,28 @@ def cars():
 @app.route('/addcar', methods=['GET', 'POST'])
 @login_required
 def add_car():
+    """ Simple page to allow user to create a new vehicle in the app. """
 
     # If a GET request send back form page
     if request.method == 'GET':
         return render_template("add_car.html")
 
-    # If a POST request check that all fields completed
-    if None in (request.form['car_name'], request.form['date_manufactured'],
-                request.form['mileage']):
-        return "ERROR: Missing required field/s on form."
+    # If a POST request check that all fields completed, otherwise flash error
+    if any(field is '' for field in [
+            request.form['car_name'], request.form['date_manufactured'],
+            request.form['mileage']
+    ]):
+        flash(u'Missing required field/s when adding new vehicle.', 'danger')
+    else:
+        # Create a new car object and add to db using add method
+        new_car = Car(
+            user_id=session["user_id"],
+            car_name=request.form['car_name'],
+            car_built=request.form['date_manufactured']).add()
 
-    # Create a new car object and add to db using add method
-    new_car = Car(
-        user_id=session["user_id"],
-        car_name=request.form['car_name'],
-        car_built=request.form['date_manufactured']).add()
-
-    # Use method to add new odometer reading for the car
-    new_car.add_odom_reading(request.form['mileage'])
-
+        # Use method to add new odometer reading for the car
+        new_car.add_odom_reading(request.form['mileage'])
+        flash(f'{new_car.car_name} added to your vehicle list.', 'primary')
     # Return to the landing screen
     return redirect('/cars')
 
@@ -189,9 +205,12 @@ def delete_car(car_id):
 
     if delete_car.user_id == session["user_id"]:
         delete_car.delete()
-        return redirect('/cars')
+        flash(f'{delete_car.car_name} deleted from your vehicle list.',
+              'primary')
+    else:
+        flash('Unauthorized access to vehicle record.', 'primary')
 
-    return "ERROR: Attempting to delete unauthorized record"
+    return redirect('/cars')
 
 
 @app.route("/car/<car_id>", methods=['GET'])
@@ -204,7 +223,8 @@ def car(car_id):
 
     # Verify the user has access to the record and that it exists
     if not car or car.user_id != session['user_id']:
-        return "ERROR: Unauthorized access"
-    
+        flash(u'Unauthorized access to vehicle record', 'danger')
+        return redirect('/cars')
+
     # Render car template
     return render_template('car.html', car=car)
--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-"""Python models defined"""
+""" auto_maint app models defined """
 import datetime
 
 from flask_sqlalchemy import SQLAlchemy
@@ -26,11 +26,12 @@ class Car(db.Model):
     user_id = db.Column(db.ForeignKey("users.user_id"), nullable=False)
     car_name = db.Column(db.String(128), nullable=False)
     car_built = db.Column(db.Date, nullable=False)
-    odo_readings = db.relationship('Odometer', cascade='all,delete', backref='user')
+    odo_readings = db.relationship(
+        'Odometer', cascade='all,delete', backref='user')
 
     def est_mileage(self):
         """
-        Returns the current estimated mileage by looking at the last odometer 
+        Returns the current estimated mileage by looking at the last odometer
         reading and comparing it to the age of the vehicle.
         """
         if not self.last_odometer():
@@ -48,6 +49,7 @@ class Car(db.Model):
         return int(estimate)
 
     def last_odometer(self):
+        """ Method to provide last odometer reading for the vehicle. """
         # Search for all odometer readings for the car.
         # Show descending / only first
         odo = Odometer.query.filter(self.car_id == Odometer.car_id).order_by(
@@ -55,23 +57,27 @@ class Car(db.Model):
         return odo
 
     def add_odom_reading(self, mileage, date=datetime.date.today()):
-        new_reading = Odometer(car_id=self.car_id, reading=mileage, reading_date=date)
+        """ Method to add an odometer reading """
+        new_reading = Odometer(
+            car_id=self.car_id, reading=mileage, reading_date=date)
         # Write to DB
         db.session.add(new_reading)
         db.session.commit()
 
     def add(self):
+        """ Method to save the current vehicle object to the DB. """
         db.session.add(self)
         db.session.commit()
         return self
-    
+
     def delete(self):
+        """ Method to delete the current vehicle object from the DB. """
         db.session.delete(self)
         db.session.commit()
 
 
 class Odometer(db.Model):
-    """Odometer reading for a car"""
+    """ Odometer reading for a vehicle. """
     __tablename__ = "odometers"
     reading_id = db.Column(db.Integer, primary_key=True)
     car_id = db.Column(db.ForeignKey("cars.car_id"), nullable=False)
@@ -79,4 +85,5 @@ class Odometer(db.Model):
     reading_date = db.Column(db.Date, nullable=False)
 
     def delete(self):
+        """ Method to delete the odomter reading. """
         db.session.delete(self)
